66c0542bdd936e4fa0c7c834e0ac2961efb575fe
julia <julia@LAPTOP-ILMLJSM->
2024-02-10 03:17:12 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+//Napisz program, który umożliwia odczytywanie długości łańcucha zapamiętanego w odpowiedniej zmiennej.
+//Program powinien mierzyć długość łańcucha nazwa miasta i województwo. Użytkownik podaje nazwę miasta i województwo.
+//Skorzystaj z metody Length, a wynik wyświetl w postaci
+//np. "Nazwa miasta Warszawa zawiera 8 liter. Nazwa województwa Mazowieckie zawiera 11 liter."
+
+class MetodaLength
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj nazwę miasta: ");
+        string nazwaMiasta = Console.ReadLine();
+
+        Console.WriteLine("Podaj nazwę województwa: ");
+        string nazwaWojewodztwa = Console.ReadLine();
+
+        int dlugoscMiasta = nazwaMiasta.Length;
+        int dlugoscWojewodztwa = nazwaWojewodztwa.Length;
+
+        Console.WriteLine($"Nazwa miasta {nazwaMiasta} zawiera {dlugoscMiasta} liter.");
+        Console.WriteLine($"Nazwa województwa {nazwaWojewodztwa} zawiera {dlugoscWojewodztwa} liter");
+    }
+}
